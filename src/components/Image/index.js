91e0f18a40d20027d79af0492a30ac2999e710d1
FabioRocha231 <fabioharoldo1@gmail.com>
2024-04-21 13:15:34 -0300

    feat: circular progress instead hardcoded loading

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -1,40 +1,43 @@
-import React, { useEffect, useState } from 'react';
-import { useImageBase64 } from '@/hooks/useImageBase64';
-import { CardMedia } from '@mui/material';
+import React from 'react'
+import { useImageBase64 } from '@/hooks/useImageBase64'
+import { CardMedia, CircularProgress } from '@mui/material'
 import Avatar from '@mui/material/Avatar'
 
 export const Base64Image = ({ mediaUrl, type }) => {
-  const imageBase64 = useImageBase64(mediaUrl);
+  const imageBase64 = useImageBase64(mediaUrl)
 
   if (type === 'post') {
     return (
       <div>
         {imageBase64 ? (
-          <CardMedia 
-              component="img"
-              height="auto"
-              image={`data:image/jpeg;base64,${imageBase64}`}
-              alt="Loading..."
+          <CardMedia
+            component="img"
+            height="auto"
+            image={`data:image/jpeg;base64,${imageBase64}`}
+            alt="Loading..."
           />
         ) : (
-          <h1>Loading...</h1>
+          <CircularProgress color="secondary" />
         )}
       </div>
-    );
+    )
   }
 
   if (type === 'avatar') {
     return (
       <div>
         {imageBase64 ? (
-          <Avatar  
-              alt="Loading..."
-              src={`data:image/jpeg;base64,${imageBase64}`}
+          <Avatar
+            alt="Loading..."
+            src={`data:image/jpeg;base64,${imageBase64}`}
           />
         ) : (
-          <h1>Loading...</h1>
+          <CircularProgress
+            color="secondary"
+            style={{ width: '15px', height: '15px' }}
+          />
         )}
       </div>
-    );
+    )
   }
-};
+}
