759f56b57d9954c601089bffbdf5d45014536156
FabioRocha231 <fabioharoldo1@gmail.com>
2024-04-21 14:07:41 -0300

    feat: finish post refactor

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,17 +1,9 @@
-import {
-  Card,
-  CardContent,
-  Divider,
-  TextField,
-  Typography,
-} from '@mui/material'
+import { Card } from '@mui/material'
 import { PostHeader } from './postHeader'
-import { Base64Image } from '../Image'
-import { PostActions } from './postActions'
 import { useHandleComments } from '@/hooks/useHandleComments'
+import { PostContent } from './postContent'
 
 export const Post = ({ post }) => {
-  const { comments, handleAddComment } = useHandleComments()
   return (
     <Card
       key={post.post_id}
@@ -24,33 +16,7 @@ export const Post = ({ post }) => {
         post_lostFound={post.post_lostFound}
         post_reward={post.post_reward}
       />
-      <CardContent>
-        <Typography variant="body1" color="text.primary" component="p">
-          {post.text}
-        </Typography>
-      </CardContent>
-      <Base64Image mediaUrl={post.post_media} type="post" />
-      <PostActions
-        comments={comments}
-        likes={post.likes}
-        shares={post.shares}
-      />
-      <Divider />
-      <Divider />
-      <CardContent>
-        <TextField
-          fullWidth
-          placeholder="Add a comment..."
-          variant="outlined"
-          size="small"
-          onKeyDown={(e) => {
-            if (e.key === 'Enter') {
-              handleAddComment(post.post_id, e.target.value.trim())
-              e.target.value = ''
-            }
-          }}
-        />
-      </CardContent>
+      <PostContent post={post} />
     </Card>
   )
 }
---
+++ src/components/post/postContent/index.js
@@ -0,0 +1,47 @@
+import { Base64Image } from '@/components/Image'
+import { CardContent, Divider, TextField, Typography } from '@mui/material'
+import { PostActions } from '../postActions'
+import { useHandleComments } from '@/hooks/useHandleComments'
+
+export const PostContent = ({ post }) => {
+  const { comments, handleAddComment } = useHandleComments()
+  return (
+    <>
+      <CardContent>
+        <Typography variant="body1" color="text.primary" component="p">
+          {post.text}
+        </Typography>
+      </CardContent>
+      <CardContent
+        style={{
+          justifyContent: 'center',
+          display: 'flex',
+        }}
+      >
+        <Base64Image mediaUrl={post.post_media} type="post" />
+      </CardContent>
+      <PostActions
+        comments={comments}
+        likes={post.likes}
+        shares={post.shares}
+      />
+      <Divider />
+      <Divider />
+
+      <CardContent>
+        <TextField
+          fullWidth
+          placeholder="Add a comment..."
+          variant="outlined"
+          size="small"
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              handleAddComment(post.post_id, e.target.value.trim())
+              e.target.value = ''
+            }
+          }}
+        />
+      </CardContent>
+    </>
+  )
+}
