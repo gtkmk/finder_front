0ef7c425d14aa373d7cd295938a95c3cd136b60d
FabioRocha231 <fabioharoldo1@gmail.com>
2024-04-20 22:22:26 -0300

    feat: usage of api interceptor

--- src/hooks/useGetPost.js
+++ src/hooks/useGetPost.js
@@ -1,28 +1,28 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { toast } from "react-toastify";
+import api from '@/services/api'
+import { useEffect, useState } from 'react'
+import { toast } from 'react-toastify'
 
 export const useGetPosts = () => {
-  const [postsData, setPostsData] = useState([]);
+  const [postsData, setPostsData] = useState([])
 
   const fetchPostsData = async () => {
     try {
-      const response = await axios.get("http://localhost:8089/posts?page=1", {
+      const response = await api.get('/posts?page=1', {
         withCredentials: true,
-      });
+      })
 
-      const { posts } = response.data.data;
-      setPostsData(posts.data);
+      const { posts } = response.data.data
+      setPostsData(posts.data)
     } catch (error) {
-      toast.error("Error fetching posts");
+      toast.error('Error fetching posts')
     }
-  };
+  }
 
   useEffect(() => {
-    fetchPostsData();
-  }, []);
+    fetchPostsData()
+  }, [])
 
   return {
     postsData,
-  };
-};
+  }
+}
