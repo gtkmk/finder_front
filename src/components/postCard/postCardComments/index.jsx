6196ef5f5c4b50d5b8f0372fef342e1565ac2ab5
FabioRocha231 <fabioharoldo1@gmail.com>
2024-05-11 18:02:59 -0300

    feat: almost finished comments zone

--- src/components/postCard/postCardComments/index.jsx
+++ src/components/postCard/postCardComments/index.jsx
@@ -3,26 +3,17 @@ import { useContext } from 'react'
 // custom components
 import { PostCardContext } from '@/contexts/postCardContext'
 
-// MUI
-import TextField from '@mui/material/TextField'
-
 // styles
 import * as S from './postCardComments.styles'
+import { CommentBox } from '../commentBox'
 
 export const PostCardComments = () => {
   const { commentsOpen } = useContext(PostCardContext)
 
   return (
     <S.PostCommentsContainer open={commentsOpen}>
-      <div>Meu ovo</div>
-      {/* <Textarea size="lg" name="Size" placeholder="Large" /> */}
-      <TextField
-        // id="outlined-multiline-static"
-        label="Comentario"
-        multiline
-        rows={2}
-        style={{ color: 'white' }}
-      />
+      <CommentBox />
+      <div>Um comentario aleatorio hardcoded</div>
     </S.PostCommentsContainer>
   )
 }
