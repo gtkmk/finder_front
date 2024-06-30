bebd31f8ad0d538080c8697e1b5bc49cf02b17fa
gtkmk <deiveaug@gmail.com>
2024-06-30 20:17:55 -0300

    feat: test

--- src/hooks/useSignInHandler.js
+++ src/hooks/useSignInHandler.js
@@ -14,30 +14,54 @@ export const useSignInHandler = () => {
     const email = formData.get('email')
     const password = formData.get('password')
 
+    // try {
+    //   const response = await fetch(`http://34.125.28.161:8089/signin`, {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify({
+    //       email,
+    //       password,
+    //     }),
+    //     credentials: 'include',
+    //   })
+
+    //   const data = await response.json()
+    //   console.log(data)
+    //   if (response.ok) {
+    //     toast.success(data.message)
+    //     setTimeout(timeOutCallback, 1000)
+    //   } else {
+    //     toast.error(data.message || 'Something went wrong. Please try again.')
+    //   }
+    // } catch (error) {
+    //   const errorMessage = error.response?.data?.message || 'Something went wrong. Please try again.'
+    //   toast.error(errorMessage)
+    // }
+
     try {
-      const response = await fetch(`http://34.125.28.161:8089/signin`, {
-        method: 'POST',
+      const response = await axios.post('http://34.125.28.161:8089/signin', {
+        email,
+        password,
+      }, {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({
-          email,
-          password,
-        }),
-        credentials: 'include',
-      })
-
-      const data = await response.json()
-      console.log(data)
-      if (response.ok) {
-        toast.success(data.message)
-        setTimeout(timeOutCallback, 1000)
+        withCredentials: true,
+      });
+    
+      const data = response.data;
+      console.log(data);
+      if (response.status === 200) {
+        toast.success(data.message);
+        setTimeout(timeOutCallback, 1000);
       } else {
-        toast.error(data.message || 'Something went wrong. Please try again.')
+        toast.error(data.message || 'Something went wrong. Please try again.');
       }
     } catch (error) {
-      const errorMessage = error.response?.data?.message || 'Something went wrong. Please try again.'
-      toast.error(errorMessage)
+      const errorMessage = error.response?.data?.message || 'Something went wrong. Please try again.';
+      toast.error(errorMessage);
     }
 
     // try {
