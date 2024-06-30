5b04f88a2f0fcfd00a65ccfbbdb22ca830604450
gtkmk <deiveaug@gmail.com>
2024-06-30 20:21:53 -0300

    feat: test

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   compiler: {
     styledComponents: true,
   },
+  output: 'standalone',
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack: (config) => {
+    // Enable polling based on env variable being set
+    if (process.env.NEXT_WEBPACK_USEPOLLING) {
+      config.watchOptions = {
+        poll: 500,
+        aggregateTimeout: 300,
+      }
+    }
+    return config
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/signin',
+        permanent: true,
+      },
+    ]
+  },
 }
 
-export default nextConfig
+module.exports = nextConfig
