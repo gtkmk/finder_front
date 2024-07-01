3049c6b2e30c8916b0e8372871a6da6fab9ee0eb
gtkmk <deiveaug@gmail.com>
2024-06-30 23:24:04 -0300

    feat: test

--- src/hooks/useSignInHandler.js
+++ src/hooks/useSignInHandler.js
@@ -14,55 +14,55 @@ export const useSignInHandler = () => {
     const email = formData.get('email')
     const password = formData.get('password')
 
-    // try {
-    //   const response = await fetch(`http://146.148.40.85:8089/signin`, {
-    //     method: 'POST',
-    //     headers: {
-    //       'Content-Type': 'application/json',
-    //     },
-    //     body: JSON.stringify({
-    //       email,
-    //       password,
-    //     }),
-    //     credentials: 'include',
-    //   })
-
-    //   const data = await response.json()
-    //   console.log(data)
-    //   if (response.ok) {
-    //     toast.success(data.message)
-    //     setTimeout(timeOutCallback, 1000)
-    //   } else {
-    //     toast.error(data.message || 'Something went wrong. Please try again.')
-    //   }
-    // } catch (error) {
-    //   const errorMessage = error.response?.data?.message || 'Something went wrong. Please try again.'
-    //   toast.error(errorMessage)
-    // }
-
     try {
-      const response = await axios.post('http://146.148.40.85:8089/signin', {
-        email,
-        password,
-      }, {
+      const response = await fetch(`http://146.148.40.85:8089/signin`, {
+        method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        withCredentials: true,
-      });
-    
-      const data = response.data;
-      console.log(data);
-      if (response.status === 200) {
-        toast.success(data.message);
-        setTimeout(timeOutCallback, 1000);
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+        credentials: 'include',
+      })
+
+      const data = await response.json()
+      console.log(data)
+      if (response.ok) {
+        toast.success(data.message)
+        setTimeout(timeOutCallback, 1000)
       } else {
-        toast.error(data.message || 'Something went wrong. Please try again.');
+        toast.error(data.message || 'Something went wrong. Please try again.')
       }
     } catch (error) {
-      const errorMessage = error.response?.data?.message || 'Something went wrong. Please try again.';
-      toast.error(errorMessage);
+      const errorMessage = error.response?.data?.message || 'Something went wrong. Please try again.'
+      toast.error(errorMessage)
     }
+
+    // try {
+    //   const response = await axios.post('http://146.148.40.85:8089/signin', {
+    //     email,
+    //     password,
+    //   }, {
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //     },
+    //     withCredentials: true,
+    //   });
+    
+    //   const data = response.data;
+    //   console.log(data);
+    //   if (response.status === 200) {
+    //     toast.success(data.message);
+    //     setTimeout(timeOutCallback, 1000);
+    //   } else {
+    //     toast.error(data.message || 'Something went wrong. Please try again.');
+    //   }
+    // } catch (error) {
+    //   const errorMessage = error.response?.data?.message || 'Something went wrong. Please try again.';
+    //   toast.error(errorMessage);
+    // }
 
     // try {
     //   const response = await axios.post('http://146.148.40.85:8089/signin', {
